6ddfd43138c68c48e12d5923684b85578995435e
Juliana R. Mancera <133284711+JulianaMancera@users.noreply.github.com>
2025-02-05 01:28:50 +0000

    OS activity

---
+++ Activity1/Act1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Enter your name:"
+read name
+echo "Enter your age:"
+read age
+echo "Enter program:"
+read program
+
+echo "Hello $name! This is yout $age and your program is $program."
