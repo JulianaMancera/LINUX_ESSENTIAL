04b626d946d58a9de19c9d457ba45eb7365395f8
Juliana R. Mancera <133284711+JulianaMancera@users.noreply.github.com>
2025-02-12 01:07:57 +0000

    OS

---
+++ Activity3/Act3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -n "Enter your name: "
+read name
+echo -n "Enter your age: "
+read age
+echo -n "Enter your favorite programming language: "
+read proglang
+
+echo "Thank you $name, you are $age years old and your favorite programming language is $proglang."
